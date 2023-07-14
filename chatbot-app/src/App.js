60d808caf3f23346bfd6a44f6637775538f1ed25
Aayog Koirala <1aayogkoirala@gmail.com>
2023-07-14 13:42:56 -0400

    Chatbot works for handling the ticket

--- chatbot-app/src/App.js
+++ chatbot-app/src/App.js
@@ -15,6 +15,8 @@ import {
   ModalCloseButton,
   ModalBody,
   IconButton,
+  FormControl, 
+  FormLabel
 } from "@chakra-ui/react";
 import { ChatIcon } from "@chakra-ui/icons";
 import "./App.css";
@@ -25,6 +27,11 @@ const App = () => {
   const [messages, setMessages] = useState([]);
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [inputMessage, setInputMessage] = useState('');
+  const [step, setStep] = useState("initial");
+  const [question, setQuestion] = useState("");
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [details, setDetails] = useState("");
 
   useEffect(() => {
     const url = `http://localhost:${process.env.REACT_APP_PORT}`;
@@ -79,6 +86,48 @@ const App = () => {
     }
   };
 
+  const handleClose = () => {
+    setStep('initial');
+    setMessages([]);
+    setInputMessage('');
+    onClose();
+  };
+
+  const handleServicesClick = () => {
+    setStep("services");
+    // do any additional setup for this step
+  };
+
+  const handleLibrarianClick = () => {
+    window.open(" https://www.lib.miamioh.edu/research/research-support/ask/", "_blank");
+  };
+
+  const handleTicketClick = () => {
+    setStep("ticket");
+    // do any additional setup for this step
+  };
+
+  const handleTicketSubmit = (e) => {
+    e.preventDefault();
+    // handle the ticket creation logic here
+    if (socket) {
+      socket.emit(
+        "createTicket",
+        {
+          question: question,
+          email: email,
+          name: name,
+          details: details,
+          ua: navigator.userAgent,
+        },
+        (responseMessage) => {
+          console.log(responseMessage);
+          setStep("initial")
+        }
+      );
+    }
+  };  
+
   return (
     <>
       <IconButton
@@ -90,8 +139,8 @@ const App = () => {
         width={50}
         height={50}
       />
-
-      <Modal isOpen={isOpen} onClose={onClose}>
+  
+      <Modal isOpen={isOpen} onClose={handleClose}>
         <ModalOverlay />
         <ModalContent
           maxW="350px"
@@ -103,36 +152,70 @@ const App = () => {
           <ModalHeader>LibChat Library Chatbot</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <Box
-              ref={chatRef}
-              borderWidth={1}
-              borderRadius="md"
-              p={3}
-              mb={3}
-              overflowY="auto"
-              height="60vh"
-            >
-              <VStack align="start" spacing={4}>
-                {messages.map((message, index) => (
-                  <Box key={index} maxW="md" p={5} rounded="md" bg={message.sender === 'user' ? 'blue.500' : 'gray.200'} alignSelf={message.sender === 'user' ? 'flex-end' : 'flex-start'}>
-                    <Text color={message.sender === 'user' ? 'white' : 'black'}>
-                      {typeof message.text === 'object' ? message.text.response.join(', ') : message.text}
-                    </Text>
-                  </Box>
-                ))}
+            {step === "initial" && (
+              <VStack>
+                <Button onClick={handleServicesClick}>Services I provide</Button>
+                <Button onClick={handleLibrarianClick}>Talk to a human librarian</Button>
+                <Button onClick={handleTicketClick}>Create a ticket for offline help</Button>
               </VStack>
-            </Box>
-            <form onSubmit={handleFormSubmit}>
-              <HStack spacing={3}>
-                <Input value={inputMessage} onChange={e => setInputMessage(e.target.value)} placeholder="Type your message..." />
-                <Button colorScheme="blue" type="submit">Send</Button>
-              </HStack>
-            </form>
+            )}
+  
+            {step === "services" && (
+              <>
+                <Box
+                  ref={chatRef}
+                  borderWidth={1}
+                  borderRadius="md"
+                  p={3}
+                  mb={3}
+                  overflowY="auto"
+                  height="60vh"
+                >
+                  <VStack align="start" spacing={4}>
+                    {messages.map((message, index) => (
+                      <Box key={index} maxW="md" p={5} rounded="md" bg={message.sender === 'user' ? 'blue.500' : 'gray.200'} alignSelf={message.sender === 'user' ? 'flex-end' : 'flex-start'}>
+                        <Text color={message.sender === 'user' ? 'white' : 'black'}>
+                          {typeof message.text === 'object' ? message.text.response.join(', ') : message.text}
+                        </Text>
+                      </Box>
+                    ))}
+                  </VStack>
+                </Box>
+                <form onSubmit={handleFormSubmit}>
+                  <HStack spacing={3}>
+                    <Input value={inputMessage} onChange={e => setInputMessage(e.target.value)} placeholder="Type your message..." />
+                    <Button colorScheme="blue" type="submit">Send</Button>
+                  </HStack>
+                </form>
+              </>
+            )}
+  
+            {step === "ticket" && (
+              <form onSubmit={handleTicketSubmit}>
+                <FormControl>
+                  <FormLabel>Name</FormLabel>
+                  <Input placeholder="Enter your name..." value={name} onChange={e => setName(e.target.value)} />
+                </FormControl>
+                <FormControl>
+                  <FormLabel>Question</FormLabel>
+                  <Input placeholder="Enter your question..." value={question} onChange={e => setQuestion(e.target.value)}/>
+                </FormControl>
+                <FormControl>
+                  <FormLabel>Details</FormLabel>
+                  <Input placeholder="Enter details about your question..." value={details} onChange={e => setDetails(e.target.value)} />
+                </FormControl>
+                <FormControl>
+                  <FormLabel>Email</FormLabel>
+                  <Input placeholder="Enter your email..." value={email} onChange={e => setEmail(e.target.value)} />
+                </FormControl>
+                <Button type="submit">Submit</Button>
+              </form>
+            )}
           </ModalBody>
         </ModalContent>
       </Modal>
     </>
   );
-};
+  };
 
 export default App;
