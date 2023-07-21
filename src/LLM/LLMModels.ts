1762796f86395f7bfb7d340e33f0e42e232ab0d6
Aayog Koirala <1aayogkoirala@gmail.com>
2023-07-21 12:31:15 -0400

    removed REDIS

--- src/LLM/LLMModels.ts
+++ src/LLM/LLMModels.ts
@@ -1,7 +1,6 @@
 import { Configuration, OpenAIApi } from "openai";
 import { PromptTemplate } from "../Prompt/PromptTemplate";
 import { ModelPromptWithTools } from "../Prompt/Prompts";
-import RedisCacheService from '../Service/redisCacheService';  // Update this with the actual import path
 
 class OpenAIModel {
   private modelConfiguration: Configuration;
@@ -9,8 +8,6 @@ class OpenAIModel {
   public readonly modelName: string = "gpt-4-0613";
   private temperature: number;
   private remainingTokens: number = 40000;
-  private cacheService: RedisCacheService;
-
   constructor(temperature = 0) {
     this.modelConfiguration = new Configuration({
       organization: "org-4LbKZFYAeYBUivA5qxcat7n6",
@@ -18,21 +15,8 @@ class OpenAIModel {
     });
     this.model = new OpenAIApi(this.modelConfiguration);
     this.temperature = temperature;
-    this.cacheService = new RedisCacheService();
   }
   
-  async getModelResponseWithCache(prompt: ModelPromptWithTools) {
-    const key = prompt.toString();
-    console.log(key[0])
-    let result = await this.cacheService.get(key);
-    
-    if (result === null) {
-      result = await this.getModelResponse(prompt);
-      await this.cacheService.set(key, result);
-    }
-
-    return result;
-  }
   /**
    * This function takes in user input and async return the AIAgent answer
    * @param inputPrompt
