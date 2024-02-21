9c1e68126856f37e733595fbb7259cf8027501e9
dominhnhut01 <dominhnhut01@gmail.com>
2024-02-21 14:29:06 -0500

    remove dangerous console.log

--- legacy/chatbot-app/src/services/GetEnvVariable.js
+++ legacy/chatbot-app/src/services/GetEnvVariable.js
@@ -1,6 +1,5 @@
 export function getEnvVariable(variableName) {
   const variable = process.env[variableName];
-  console.log(process.env)
   if (variable === undefined) {
     throw new Error(`Variable ${variableName} is not defined in .env`);
   }
