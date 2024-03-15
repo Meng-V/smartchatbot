b14d44ba9416f325cc10f28a050ace9396a12782
Steven Do <62295931+dominhnhut01@users.noreply.github.com>
2024-03-15 12:51:18 -0400

    98 create librarian subject tool (#99)
    * finish adding libapps authorization, librarian subject tool and unit tests
    
    * improve the libapps authorization service to avoid cases where keys are not initialized in time
    
    * adjust github action .yml file to include more environment variable
    
    * adjust service name for librarian subject
    
    * prettier fix format

--- new-chatbot-app/src/llm-chain/llm-toolbox/llm-tool.interface.ts
+++ new-chatbot-app/src/llm-chain/llm-toolbox/llm-tool.interface.ts
@@ -1,25 +1,14 @@
 export type LlmToolInput = Record<string, string | null>;
 
 export interface LlmTool {
-  /**
-   * Name of the tool
-   */
-  name: string;
-  /**
-   * Detailed description of the tool
-   */
-  description: string;
+  readonly toolName: string;
+  readonly toolDescription: string;
+  readonly toolParametersStructure: { [parameterName: string]: string };
 
   /**
-   * Parameters that the tool accepts
-   */
-  parameters: {
-    [parameterName: string]: string;
-  };
-
-  /**
-   * Method for LLM agent to use
+   * Interface method for LLM agent to use
    * @param input input that the LLM model inputs to the tool
+   * @returns the result from the tools in the readable string
    */
-  toolRunForLlm(input: LlmToolInput): Promise<string>; //
+  toolRunForLlm(llmToolInput: LlmToolInput): Promise<string>;
 }
--- new-chatbot-app/src/llm-chain/llm-toolbox/llm-toolbox.module.ts
+++ new-chatbot-app/src/llm-chain/llm-toolbox/llm-toolbox.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { LibrarianSubjectLookupToolService } from './libapps-tools/librarian-subject-lookup-tool/librarian-subject-lookup-tool.service';
+import { LibraryApiModule } from '../../library-api/library-api.module';
+import { HttpModule } from '@nestjs/axios';
 
-@Module({})
+@Module({
+  imports: [LibraryApiModule, HttpModule],
+  providers: [LibrarianSubjectLookupToolService],
+})
 export class LlmToolboxModule {}
