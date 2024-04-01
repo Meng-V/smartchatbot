d4006f6bdb8e69c372f02c992a42990e36583086
Loc Tran <vinhloctranhuu@gmail.com>
2024-04-01 13:54:53 -0400

    Deleting Cancel Reservation service. Leave alone Libcal Auth to continute for Room Reservation. Will add Cancel Reser later in another branch

--- new-chatbot-app/src/llm-chain/llm-toolbox/llm-toolbox.module.ts
+++ new-chatbot-app/src/llm-chain/llm-toolbox/llm-toolbox.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { LibrarianSubjectLookupToolService } from './libapps-tools/librarian-subject-lookup-tool/librarian-subject-lookup-tool.service';
-import { CancelReservationToolService } from './libcal-tools/cancel-reservation-tool/cancel-reservation-tool.service';
+// import { CancelReservationToolService } from './libcal-tools/cancel-reservation-tool/cancel-reservation-tool.service';
 import { LibraryApiModule } from '../../library-api/library-api.module';
 import { HttpModule } from '@nestjs/axios';
 import { CitationAssistToolService } from './citation-assist-tool/citation-assist-tool.service';
@@ -11,12 +11,12 @@ import { SharedModule } from '../../shared/shared.module';
   imports: [LibraryApiModule, HttpModule, SharedModule],
   providers: [
     LibrarianSubjectLookupToolService,
-    CancelReservationToolService,
+    // CancelReservationToolService,
     CitationAssistToolService],
   exports: [
     LibrarianSubjectLookupToolService,
     CitationAssistToolService,
-    CancelReservationToolService
+    // CancelReservationToolService
   ],
 })
 export class LlmToolboxModule {}
