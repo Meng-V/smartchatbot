35bcc9963f5ea143a142e7d789e7d88c245c8feb
Loc Tran <vinhloctranhuu@gmail.com>
2024-04-02 14:17:13 -0400

    Add Room Cancel Reservation tool for Libcal API

--- new-chatbot-app/src/llm-chain/llm-toolbox/llm-toolbox.module.ts
+++ new-chatbot-app/src/llm-chain/llm-toolbox/llm-toolbox.module.ts
@@ -1,14 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 import { LibrarianSubjectLookupToolService } from './libapps-tools/librarian-subject-lookup-tool/librarian-subject-lookup-tool.service';
 import { LibraryApiModule } from '../../library-api/library-api.module';
-import { HttpModule } from '@nestjs/axios';
 import { CitationAssistToolService } from './citation-assist-tool/citation-assist-tool.service';
+import { CancelReservationToolService } from './libcal-tools/cancel-reservation-tool/cancel-reservation-tool.service';
 import { SharedModule } from '../../shared/shared.module';
 
 @Module({
   imports: [LibraryApiModule, HttpModule, SharedModule],
-  providers: [LibrarianSubjectLookupToolService, CitationAssistToolService],
-  exports: [LibrarianSubjectLookupToolService, CitationAssistToolService],
+  providers: [
+    LibrarianSubjectLookupToolService,
+    CitationAssistToolService,
+    CancelReservationToolService,
+  ],
+  exports: [
+    LibrarianSubjectLookupToolService,
+    CitationAssistToolService,
+    CancelReservationToolService,
+  ],
 })
 export class LlmToolboxModule {}
