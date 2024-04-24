011f42d35a1ff22b4ccd2375a63881e46d80f485
Lam Nguyen <nguyenlm@miamioh.edu>
2024-04-24 12:29:57 -0400

    Form now resets after submitting

--- new-chatbot-app/client/src/components/FeedbackFormComponent.jsx
+++ new-chatbot-app/client/src/components/FeedbackFormComponent.jsx
@@ -21,7 +21,6 @@ import { SocketContext } from '../context/SocketContextProvider';
 import { MessageContext } from '../context/MessageContextProvider';
 
 const FeedbackFormComponent = () => {
-
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [hover, setHover] = useState(null);
   const [rating, setRating] = useState(0);
@@ -32,22 +31,22 @@ const FeedbackFormComponent = () => {
   const handleFormOpen = () => {
     onOpen();
     setHover(null);
-    setRating(0);
-  }
+  };
 
   const handleRating = (curRating) => {
     setRating(curRating);
-  }
+  };
 
   const handleFormSubmit = () => {
-    socketContextValues.sendUserFeedback(
-      {
-        userRating: rating,
-        userComment: details,
-      }
-    );
+    socketContextValues.sendUserFeedback({
+      userRating: rating,
+      userComment: details,
+    });
+    setRating(0);
+    setDetails(undefined);
+    setHover(null);
     onClose();
-  }
+  };
 
   return (
     <>
@@ -62,35 +61,31 @@ const FeedbackFormComponent = () => {
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader
-            display="flex"
-            alignItems="center"
-            ps={2}
-          >
+          <ModalHeader display="flex" alignItems="center" ps={2}>
             <img
               src="https://libapps.s3.amazonaws.com/accounts/190074/images/0721_STier1_Libraries_HS_186KW_K_Digital.png"
               height={50}
               width={120}
               alt="library logo"
             />
-            <Box ml={3}>
-              Smart Chatbot
-            </Box>
+            <Box ml={3}>Smart Chatbot</Box>
           </ModalHeader>
           <ModalBody>
             <form>
               <FormControl>
-                <FormLabel>
-                  Rate this conversation
-                </FormLabel>
-                <HStack spacing={"2px"}>
+                <FormLabel>Rate this conversation</FormLabel>
+                <HStack spacing={'2px'}>
                   {[...Array(5)].map((star, index) => {
                     const ratingValue = index + 1;
                     return (
                       <Box
                         as="label"
                         key={index}
-                        color={ratingValue <= (hover || rating) ? "#ffc107" : "#e4e5e9"}
+                        color={
+                          ratingValue <= (hover || rating)
+                            ? '#ffc107'
+                            : '#e4e5e9'
+                        }
                         onMouseEnter={() => setHover(ratingValue)}
                         onMouseLeave={() => setHover(null)}
                       >
@@ -101,7 +96,7 @@ const FeedbackFormComponent = () => {
                           display="none"
                         />
                         <FaStar
-                          cursor={"pointer"}
+                          cursor={'pointer'}
                           size={20}
                           transition="color 200ms"
                         />
@@ -118,26 +113,16 @@ const FeedbackFormComponent = () => {
                   onChange={(e) => setDetails(e.target.value)}
                 />
               </FormControl>
-              <Text
-                color="red"
-                fontSize={14}
-                as="i"
-              >
+              <Text color="red" fontSize={14} as="i">
                 * Submitting this form will restart your session!
               </Text>
             </form>
           </ModalBody>
           <ModalFooter>
-            <Button
-              mr={3}
-              onClick={onClose}
-            >
+            <Button mr={3} onClick={onClose}>
               Close
             </Button>
-            <Button
-              colorScheme="red"
-              onClick={handleFormSubmit}
-            >
+            <Button colorScheme="red" onClick={handleFormSubmit}>
               Submit
             </Button>
           </ModalFooter>
@@ -145,6 +130,6 @@ const FeedbackFormComponent = () => {
       </Modal>
     </>
   );
-}
+};
 
 export default FeedbackFormComponent;
--- new-chatbot-app/client/src/context/MessageContextProvider.jsx
+++ new-chatbot-app/client/src/context/MessageContextProvider.jsx
@@ -1,18 +1,17 @@
-import { createContext, useState, useMemo } from "react";
+import { createContext, useState, useMemo } from 'react';
 
 const MessageContext = createContext();
 
-const MessageContextProvider = ({children}) => {
-
+const MessageContextProvider = ({ children }) => {
   const [message, setMessage] = useState([]);
-  const [inputMessage, setInputMessage] = useState("");
+  const [inputMessage, setInputMessage] = useState('');
   const [isTyping, setIsTyping] = useState(false);
 
   const resetState = () => {
     setMessage([]);
-    setInputMessage("");
+    setInputMessage('');
     setIsTyping(false);
-  }
+  };
 
   const addMessage = (message, sender, id = undefined) => {
     const messageText =
@@ -20,29 +19,34 @@ const MessageContextProvider = ({children}) => {
         ? message.response.join('\n')
         : message;
     setMessage((prevMessages) => {
-      const updatedMessages = [...prevMessages, { text: messageText, sender, messageId: id }];
+      const updatedMessages = [
+        ...prevMessages,
+        { text: messageText, sender, messageId: id },
+      ];
       sessionStorage.setItem('chat_messages', JSON.stringify(updatedMessages));
       return updatedMessages;
     });
   };
 
-  const messageContextValues = useMemo(() => ({
-    message,
-    setMessage,
-    inputMessage,
-    setInputMessage,
-    isTyping,
-    setIsTyping,
-    addMessage,
-    resetState
-  }), [message, inputMessage, isTyping]);
+  const messageContextValues = useMemo(
+    () => ({
+      message,
+      setMessage,
+      inputMessage,
+      setInputMessage,
+      isTyping,
+      setIsTyping,
+      addMessage,
+      resetState,
+    }),
+    [message, inputMessage, isTyping],
+  );
 
   return (
-    <MessageContext.Provider value={{messageContextValues}}>
+    <MessageContext.Provider value={{ messageContextValues }}>
       {children}
     </MessageContext.Provider>
-  )
+  );
+};
 
-}
-
-export { MessageContext, MessageContextProvider};
+export { MessageContext, MessageContextProvider };
