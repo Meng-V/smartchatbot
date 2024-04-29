4cdf7596399a78aed21dffe1209a9a0e964f6762
Steven Do <62295931+dominhnhut01@users.noreply.github.com>
2024-04-29 19:02:55 -0400

    remove legacy code and move the Nest.js code to the front (#135)
    * remove legacy code and move the Nest.js code to the front
    
    * fix github action to address new directory
    
    * fix dockerfile to address new directory
    
    * fix ci-cd so it only create new docker images on push on main branch

--- legacy/chatbot-app/src/services/GetEnvVariable.js
+++
@@ -1,7 +0,0 @@
-export function getEnvVariable(variableName) {
-  const variable = process.env[variableName];
-  if (variable === undefined) {
-    throw new Error(`Variable ${variableName} is not defined in .env`);
-  }
-  return variable;
-}
