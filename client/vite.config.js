79a49c6527a0e9142dfa7d8839a3ea390bfd0927
Loc Tran <vinhloctranhuu@gmail.com>
2024-09-12 16:15:56 -0400

    Update base URL in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => {
       strictPort: true,
     },
     server: {
-      origin: JSON.stringify(env.VITE_BACKEND_URL_PROD),
+      origin: JSON.stringify(env.VITE_BACKEND_URL),
       port: 5173,
       host: true,
     },
